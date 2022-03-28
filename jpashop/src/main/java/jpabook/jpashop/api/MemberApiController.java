07434f4ae92e3aa76285d3d0da936e8ec03a45f3
Boeun's <rnqhdms@gmail.com>
2022-03-28 13:39:34 +0900

    api 설계

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,11 +3,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,6 +18,7 @@ public class MemberApiController {
 
 
     /**
+     * 회원 가입
      * V1 : 요청 값으로 Member 엔티티를 직접 받는다.
      * 문제점 : 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
      * Entity에 API 검증을 위한 로직이 들어간다.(@NotEmpty 등등)
@@ -35,7 +35,8 @@ public class MemberApiController {
     }
 
     /**
-     * V2 : 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다. -> 로직 분리
+     * 회원가입
+     * V2 : 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.-> 로직 분리
      * @param request
      * @return
      */
@@ -48,6 +49,28 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
 
+    }
+
+    /**
+     * 회원 수정
+     */
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id")Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request){
+
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
     }
 
     @Data
