2dce8551fef3e7169e5e9980ba57f0aa89b61cdb
Boeun's <rnqhdms@gmail.com>
2022-04-23 15:58:23 +0900

    필드와 컬럼 맾ㅇ

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,38 +1,40 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
-@Entity //Jpa가 관리하는 클래스로 인식
+//Jpa가 관리하는 클래스로 인식
+@Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
 
-    public Member() {
+    @Column(name ="name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    //enum타입으로 DB에 매칭
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    //DB에서 (날짜-시간)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    //큰 컨텐츠 사이즈를 사용 할때
+    private String description;
+
+    public Member(){
+
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 
 }
